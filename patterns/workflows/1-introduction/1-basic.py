b62a20460ad31d6e320652c5f1b5f620edcd1129
Rowingbobcat <julien.gattoni@gmail.com>
2025-02-07 23:20:34 +0100

    Change to 4o

--- patterns/workflows/1-introduction/1-basic.py
+++ patterns/workflows/1-introduction/1-basic.py
@@ -4,15 +4,12 @@ from openai import OpenAI
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-
 completion = client.chat.completions.create(
-    model="gpt-4",
+    model="gpt-4o",
+    store=True,
     messages=[
         {"role": "system", "content": "You're a helpful assistant."},
-        {
-            "role": "user",
-            "content": "Write a limerick about the Python programming language.",
-        },
+        {"role": "user", "content": "write a haiku about ai"},
     ],
 )
 
